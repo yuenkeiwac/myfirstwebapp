374a65704f27881a2807de73a6a89142385e274a
yuenkeiwac <97879052+yuenkeiwac@users.noreply.github.com>
2022-04-26 15:53:36 +0800

    Update sentimentAnalysis.py

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -14,4 +14,5 @@ if readme:
         This is a web app demo using [streamlit](https://streamlit.io/) library. It is hosted on [heroku](https://www.heroku.com/). You may get the codes via [github](https://github.com/yuenkeiwac/myfirstwebapp/sentimentAnalysis.py)
         """)
  
-title = st.text_input('Enter a sentence', 'Life of Brian')
+title = st.text_input('Enter a sentence', 'I feel good today')
+
