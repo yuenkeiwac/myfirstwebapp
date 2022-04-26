d20cfeb51c3fcdf3ffb387a731cbdb10a6f39a7e
yuenkeiwac <97879052+yuenkeiwac@users.noreply.github.com>
2022-04-26 16:33:49 +0800

    Create sentimentAnalysis.py

---
+++ sentimentAnalysis.py
@@ -0,0 +1,22 @@
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from termcolor import colored
+import re
+vad = SentimentIntensityAnalyzer()
+
+def Compute_ENGWord_Sentiment(word):
+polarity_value = 0
+
+# Vader Lexicon
+try:
+    polarity_value = vad.polarity_scores(re.sub('-', ' ', word))['compound']
+except Exception as e:
+    print(e, word)
+    polarity_value = 0
+
+return polarity_value
+
+def split_sentence(sent):
+score = 0.0
+for word in sent.split():
+score += Compute_ENGWord_Sentiment(word)
+return score
