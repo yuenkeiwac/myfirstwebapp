4b3baf9f677230bd54206d13839dbfe137ad6c93
yuenkeiwac <97879052+yuenkeiwac@users.noreply.github.com>
2022-04-26 17:09:50 +0800

    Update sentimentAnalysis.py

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -4,19 +4,19 @@ import re
 vad = SentimentIntensityAnalyzer()
 
 def Compute_ENGWord_Sentiment(word):
-polarity_value = 0
-
-# Vader Lexicon
-try:
-    polarity_value = vad.polarity_scores(re.sub('-', ' ', word))['compound']
-except Exception as e:
-    print(e, word)
     polarity_value = 0
 
-return polarity_value
+    # Vader Lexicon
+    try:
+        polarity_value = vad.polarity_scores(re.sub('-', ' ', word))['compound']
+    except Exception as e:
+        print(e, word)
+        polarity_value = 0
+
+    return polarity_value
 
 def split_sentence(sent):
-score = 0.0
-for word in sent.split():
-score += Compute_ENGWord_Sentiment(word)
-return score
+    score = 0.0
+    for word in sent.split():
+        score += Compute_ENGWord_Sentiment(word)
+    return score
