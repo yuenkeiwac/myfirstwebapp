257474b30890f782feb6a440a5b5bf9d35435ebc
yuenkeiwac <97879052+yuenkeiwac@users.noreply.github.com>
2022-04-27 09:03:14 +0800

    Update sentimentAnalysisDemo.py

--- sentimentAnalysisDemo.py
+++ sentimentAnalysisDemo.py
@@ -8,7 +8,7 @@ import nltk
 nltk.download('vader_lexicon')
 
 local_css("style.css")
-
+    
 st.header("Sentiment Analysis")
 
 readme = st.checkbox("readme first")
@@ -23,27 +23,25 @@ option = st.sidebar.selectbox(
     'Language',
      ['English','Chinese'])
 
-text = st.empty() 
-sentence = text.text_input('Enter a sentence', '', key='1')
-
 predict = Prediction()
+
 if option == 'English':
-   score = predict.split_sentence(sentence)
+    text = st.empty() 
+    sentence = text.text_input('Enter a sentence', '', key='1')
+    score = predict.split_sentence(sentence)
+    color, sentiment = predict.returnSentiment(score)
+    t = "<div>"+ sentence + "<span class='" + color + "'>   " + sentiment+ "</span></div>"
+    sentence = text.text_input('Enter a sentence', '', key='2')
+    st.markdown(t, unsafe_allow_html=True)
 elif option == 'Chinese':
-   score = predict.Compute_CHIWord_Sentiment(sentence)
-
-if (score < 0):
-    color = 'highlight red'
-    sentiment = 'Negative'
-elif (score > 0):
-    color = 'highlight green'
-    sentiment = 'Positive'
-else:
-    color = 'highlight orange'
-    sentiment = 'Neutral'
-
-t = "<div>"+ sentence + "<span class='" + color + "'>   " + sentiment+ "</span></div>"
-sentence = text.text_input('Enter a sentence', '', key='2')
+    text = st.empty() 
+    sentence = text.text_input('Enter a sentence', '', key='1')
+    score = predict.Compute_CHIWord_Sentiment(sentence)
+    color, sentiment = predict.returnSentiment(score)
+    t = "<div>"+ sentence + "<span class='" + color + "'>   " + sentiment+ "</span></div>"
+    sentence = text.text_input('Enter a sentence', '', key='2')
+    st.markdown(t, unsafe_allow_html=True)
+    
 #t = "<div>Hello there my <span class='highlight blue'>name <span class='bold'>yo</span> </span> is <span class='highlight red'>Fanilo <span class='bold'>Name</span></span></div>"
 
-st.markdown(t, unsafe_allow_html=True)
+
